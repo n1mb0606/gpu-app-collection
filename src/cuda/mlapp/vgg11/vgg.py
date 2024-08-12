dde2fa0c3fb9d64a84c380504f070e8e04bdbcd1
n1mb0606 <n1mb0606@gmail.com>
2024-08-12 15:09:27 +0900

    Add vggnet

---
+++ src/cuda/mlapp/vgg11/vgg.py
@@ -0,0 +1,8 @@
+import torch
+from torchvision.models.vgg import vgg11
+from torchvision.models.vgg import VGG11_Weights
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = vgg11(weights=VGG11_Weights.DEFAULT).to(device)
+inputs = torch.randn(1,3,224,224).to(device)
+y = model(inputs)
---
+++ src/cuda/mlapp/vgg11/vgg.sh
@@ -0,0 +1,1 @@
+python3 $BINDIR/$BINSUBDIR/vgg.py
