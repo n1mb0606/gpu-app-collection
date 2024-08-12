691202d63098ea2692b267f6fe8b25e36f918b3d
n1mb0606 <n1mb0606@gmail.com>
2024-08-12 22:06:40 +0900

    Add vit

---
+++ src/cuda/mlapp/vit/vit.py
@@ -0,0 +1,8 @@
+import torch
+from torchvision.models.vision_transformer import vit_b_16
+from torchvision.models.vision_transformer import ViT_B_16_Weights
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = vit_b_16(weights=ViT_B_16_Weights.DEFAULT).to(device)
+inputs = torch.randn(1,3,224,224).to(device)
+y = model(inputs)
