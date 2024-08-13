2bdbbf1b7503d2edca7c628d118e897b4899d599
n1mb0606 <n1mb0606@gmail.com>
2024-08-13 20:13:02 +0900

    Add vit

--- src/cuda/mlapp/mobilenetv3_small/mobilenet.py
+++ src/cuda/mlapp/mobilenetv3_small/mobilenet.py
@@ -5,4 +5,5 @@ from torchvision.models.mobilenetv3 import MobileNet_V3_Small_Weights
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = mobilenet_v3_small(weights=MobileNet_V3_Small_Weights.DEFAULT).to(device)
 inputs = torch.randn(1,3,224,224).to(device)
-y = model(inputs)
+with torch.inference_mode():
+    y = model(inputs)
--- src/cuda/mlapp/resnet50/resnet.py
+++ src/cuda/mlapp/resnet50/resnet.py
@@ -5,4 +5,5 @@ from torchvision.models.resnet import ResNet50_Weights
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = resnet50(weights=ResNet50_Weights.DEFAULT).to(device)
 inputs = torch.randn(1,3,224,224).to(device)
-y = model(inputs)
+with torch.inference_mode():
+    y = model(inputs)
